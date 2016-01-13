f36a012686aa16919a1593c6fff115cece76c321
zgulde <zachgulde@gmail.com>
2016-01-13 15:35:38 -0600

    updated seeder to grab a user id from the db instead of hardcoded id

--- app/database/seeds/PostTableSeeder.php
+++ app/database/seeds/PostTableSeeder.php
@@ -4,11 +4,13 @@ class PostTableSeeder extends Seeder
 {
     public function run()
     {
+        $user = User::first();
+
         $post = new Post();
         $post->title = 'test seeder post';
         $post->image = '';
         $post->content = 'lorem';
-        $post->user_id = 1;
+        $post->user_id = $user->id;
 
         $post->save();
     }
